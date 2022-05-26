f693a07604e53a17f4ceb43de0a0c3a603012b2f
yaswanthk393 <105685048+yaswanthk393@users.noreply.github.com>
2022-05-25 22:27:27 -0400

    Update part_1h.sql

--- part_1h.sql
+++ part_1h.sql
@@ -3,5 +3,5 @@ select d.Dlocation
 from dept_locations as d
 where d.Dnumber =(select pr.Dnum from works_on as wo,project as pr
 where wo.Pno=pr.Pnumber
- group by pr.Dnum 
- order by SUM(wo.Hours) limit 1);
+group by pr.Dnum 
+order by SUM(wo.Hours) limit 1);
